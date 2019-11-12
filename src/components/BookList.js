9c9cff713754e073eb1c85bf931cad65db27a1ac
keshava kotri <keshav143420@gmail.com>
2019-11-12 12:04:06 +0530

    added book price to card

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -6,7 +6,6 @@ class BookList extends React.Component {
 
   render() {
     return (
-
       <CardColumns>
         {this.props.books.map(book =>
           <Card key={book.id}  >
@@ -16,11 +15,11 @@ class BookList extends React.Component {
               <Card.Text>{book.description.slice(0, 50) + '...'}
               </Card.Text>
             </Card.Body>
-            <Card.Footer>
-              <Button>buy</Button>
+            <Card.Footer style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button>Add to Cart</Button>
+              <Button variant="light" disabled> &#x20b9; {book.price}</Button>
             </Card.Footer>
           </Card>)}
-
       </CardColumns>
     );
   }
