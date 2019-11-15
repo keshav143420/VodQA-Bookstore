41dcbb88b9a56969d5fd78688c23259a426ed2a7
keshava kotri <keshav143420@gmail.com>
2019-11-15 19:18:05 +0530

    updated error messages for login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -17,6 +17,12 @@ export function LoginPage() {
   let location = useLocation();
   let { from } = location.state || { from: { pathname: "/" } };
 
+  const isEmailValid = (email) => {
+    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*$/.test(email);
+  }
+  const isPasswordValid = (password) => {
+    return password && password.length > 0;
+  }
   const handleSubmit = event => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
@@ -51,16 +57,21 @@ export function LoginPage() {
             <Form.Group as={Col} md="12" controlId="validationCustom03">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" placeholder="Email" defaultValue={email} required onChange={e => setEmail(e.target.value)} />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid email.
+              {
+                !isEmailValid(email) && <Form.Control.Feedback type="invalid">
+                  Please provide a valid email.
               </Form.Control.Feedback>
+              }
             </Form.Group>
             <Form.Group as={Col} md="12" controlId="validationCustom04">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" defaultValue={password} required onChange={e => setPassword(e.target.value)} />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid password. (atleast 1 char)
-              </Form.Control.Feedback>
+              {
+                !isPasswordValid(password) &&
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid password. (atleast 1 char)
+                </Form.Control.Feedback>
+              }
             </Form.Group>
           </Form.Row>
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
